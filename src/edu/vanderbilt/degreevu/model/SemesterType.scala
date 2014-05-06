aa89de308ccf83b1ec19e92f91dd6e76658c8631
AliceCengal <cengallut@gmail.com>
2014-05-06 16:32:28 -0500

    Idiot...

--- src/edu/vanderbilt/degreevu/model/SemesterType.scala
+++ src/edu/vanderbilt/degreevu/model/SemesterType.scala
@@ -11,18 +11,18 @@ object SemesterType {
 
   val SPRING     = SemesterType("Spring", 1)
   val FALL       = SemesterType("Fall", 5)
-  val SUMMER_ONE = SemesterType("Summer I", 2)
-  val SUMMER_TWO = SemesterType("Summer II", 3)
-  val MAYMESTER  = SemesterType("Maymester", 4)
+  val SUMMER_ONE = SemesterType("Summer I", 3)
+  val SUMMER_TWO = SemesterType("Summer II", 4)
+  val MAYMESTER  = SemesterType("Maymester", 2)
 
   /**
    * Return all SemesterTypes in chronological order.
    */
   def all: Seq[SemesterType] = List(
                                      SPRING,
+                                     MAYMESTER,
                                      SUMMER_ONE,
                                      SUMMER_TWO,
-                                     MAYMESTER,
                                      FALL)
 
 }
